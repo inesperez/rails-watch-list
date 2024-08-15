629c9e758073537b39e43f487bddbeef90728475
Ines Perez <inesperezgomez@outlook.com>
2024-08-15 16:38:34 +0100

    added bookmarks

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
@@ -9,5 +9,8 @@ Rails.application.routes.draw do
   # root "posts#index"
   root "lists#index"
 
-  resources :lists, only:[:show, :new, :create]
+  resources :lists, only:[:show, :new, :create] do
+    resources :bookmarks, only:[:index, :new, :create]
+  end
+  resources :bookmarks, only:[:destroy]
 end
